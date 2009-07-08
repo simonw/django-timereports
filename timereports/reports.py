97861f75078ab57bda88fbb1161c09266adbeb96
Simon Willison <simon@simonwillison.net>
2009-07-08 17:20:27 +0100

    CreatedObjectsReport base class

--- timereports/reports.py
+++ timereports/reports.py
@@ -1,4 +1,5 @@
-from base import Report, Hourly
+from base import Report, Hourly, Daily
+from django.db.models import Min
 import os, re
 
 load_average_re = re.compile('load average: ([\d.]+)')
@@ -12,16 +13,36 @@ class LoadAverage(Report):
         s = os.popen('uptime').read().strip()
         return int(100 * float(load_average_re.search(s).group(1)))
 
-class CreatedUsers(Report):
-    slug = 'user_accounts'
-    name = 'User accounts'
+class CreatedObjectsReport(Report):
+    model = None
+    created_field = None
     frequency = Hourly()
     
     def earliest_date(self):
-        from django.db.models import Min
-        from expenses.models import User
-        return User.objects.aggregate(x = Min('created'))['x']
+        return self.model.objects.aggregate(x = Min(self.created_field))['x']
 
     def value_at(self, dt):
-        from expenses.models import User
-        return User.objects.filter(created__lt = dt).count()
+        return self.model.objects.filter(**{
+            '%s__lt' % self.created_field: dt
+        }).count()
+
+from expenses.models import User
+class CreatedUsers(CreatedObjectsReport):
+    slug = 'user_accounts'
+    name = 'User accounts'
+    model = User
+    created_field = 'created'
+    frequency = Daily()
+
+from expenses.models import Vote, Page
+class ReviewedPages(Report):
+    slug = 'reviewed_pages'
+    name = 'Reviewed pages'
+    frequency = Daily()
+    
+    def earliest_date(self):
+        return Vote.objects.aggregate(x = Min('created'))['x']
+
+    def value_at(self, dt):
+        return Vote.objects.filter(created__lte = dt).values('page').distinct().count()
+
